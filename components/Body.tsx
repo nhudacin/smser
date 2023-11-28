bc7a595d607e56785ed30a0775415c823a75d5a9
Nick Hudacin <nhudacin@outlook.com>
2023-11-28 15:43:13 -0600

    fixing spacing

--- components/Body.tsx
+++ components/Body.tsx
@@ -200,7 +200,7 @@ const Body = ({
                 <p className="fixed ml-0 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:ml-4">
                     Making group text management a little easier
                 </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <div className="fixed bottom-0 left-0 flex h-0 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
                     <div className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0" >
                         By{' '}
                         <Image
