25a6503dcb2957ded8075a683cac6a6f1682c03b
Nick Hudacin <nhudacin@outlook.com>
2024-09-25 15:54:38 -0500

    some of vercel has been removed

--- build/psake.ps1
+++ build/psake.ps1
@@ -1,3 +1,29 @@
+properties {
+    $repoRoot = Split-Path -Parent $PSScriptRoot
+}
+
 task Build {
     Write-Output "Building..."
+}
+
+task RunDev {
+    Write-Output "Running Dev..."
+
+    # ensure azure is running
+    try {
+        Invoke-RestMethod "http://127.0.0.1:10000" -Method Get
+    }
+    catch {
+        if ( $_.ErrorDetails.Message -match "InvalidQueryParameterValue") {
+            Write-Output "Azurite Already Running!"
+        }
+        else {
+            Write-Output "Starting Azurite..."
+            $null = azurite --silent --location c:\azurite --debug c:\azurite\debug.log &
+        }
+    }
+
+    # cd to src dir
+    Set-Location "$repoRoot/src"
+    npm run dev
 }
